4cf661638b680f8b9d20cb0e26ff0696507c6b57
thiaguiarfideles <30662500+thiaguiarfideles@users.noreply.github.com>
2024-04-05 11:18:47 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,6 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
